d1c06eb2de3e0d4cffaec80dd94d052fb9469f6c
eslam-ebrahem <eslam230@gmail.com>
2024-05-12 16:06:16 +0200

    do while

---
+++ lesson19/lib/main.dart
@@ -0,0 +1,11 @@
+import "dart:io";
+
+// eslam ebrahem >>>>>>>>>>>>>>>>>>> do while
+void main() {
+  int i = 0;
+  do {
+    stdout.writeln(i);
+    stdout.writeln("next");
+    i++;
+  } while (i <= 20);
+}
